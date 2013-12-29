a142cbcbd9cc64bf052a86441c77f2074c2c9d1f
Graham Gilchrist <mail@grahamgilchrist.com>
2013-12-29 20:14:04 +0000

    Initial JS file

---
+++ ckeditor_custom_config.js
@@ -0,0 +1,12 @@
+/*
+  Custom configuration for ckeditor.
+ 
+  Configuration options can be set here. Any settings described here will be
+  overridden by settings defined in the $settings variable of the hook. To
+  override those settings, do it directly in the hook itself to $settings.
+*/
+CKEDITOR.editorConfig = function(config) {
+    //Set any required additonal config settings here
+    //e.g.
+    // config.setting = value;
+};
